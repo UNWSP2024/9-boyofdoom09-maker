7c5b48705ad2a7074c4fa2ccfeec5bf9993abda3
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-10-28 13:45:13 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -3,3 +3,13 @@
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+import random
+Range = int(input('How many random numbers would you like in your file? ',))
+
+if Range < 1000:
+    for i in range(Range):
+        bob = random.randint(1,500)
+        print(bob)
+else: 
+    print('The number must be less than 1,000')
+# come back to this one and Don't forget to put the stuff in a file (thumbs up)
